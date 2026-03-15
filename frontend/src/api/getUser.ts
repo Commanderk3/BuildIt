bb7b968af4b6f61a943fe8c74c30370d8d85378b
Commanderk3 <diwangshukakoty@gmail.com>
2026-03-15 21:51:38 +0530

    feat: update API endpoints to production URLs for authentication and project management

--- frontend/src/api/getUser.ts
+++ frontend/src/api/getUser.ts
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 
 export async function getUserDetails() {
   try {
-    const res = await axios.get("http://localhost:3000/user/me", {
+    const res = await axios.get("https://l012eckn68.execute-api.ap-south-1.amazonaws.com/user/me", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
