bf70d30e76cdd41c63db2e4438b58d40b0b253ad
Commanderk3 <diwangshukakotygmail.com>
2026-03-17 15:14:21 +0530

    server url added

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000";
+const API = "https://l012eckn68.execute-api.ap-south-1.amazonaws.com";
 
 const sendOtp = async (email: string) => {
   try {
